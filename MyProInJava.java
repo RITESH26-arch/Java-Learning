90ce2156936dc27ee3816ffbb6e5de7698f2b281
RITESH26-arch <rdongave2@gmail.comgit config --global --listgit config --global user.email rdongave2@gmail.com>
2026-07-14 19:31:53 +0530

    Java Set-up and first few programs

---
+++ MyProInJava.java
@@ -0,0 +1,12 @@
+public class MyProInJava { 
+    public static void main(String[] args){
+        String Name = "Ritesh Kalyan Dongave";
+        int Rollno = 26;
+        double CGPA = 7.5;
+
+        System.out.println("Name = "+Name);
+        System.out.println("Roll no = "+Rollno);
+        System.out.println("CGPA = "+CGPA);
+    }
+    
+}
---
+++ main.java
@@ -0,0 +1,16 @@
+import javax.swing.plaf.TreeUI;
+
+public class main{
+    public static void main(String[] args){
+        System.out.println("Heloo Bhen Ke Lode");
+        int a = 26;
+        double price = 26.12;
+        String name = "Ritesh";
+        boolean isStudent = true;
+        System.out.println("a"+a);
+        System.out.println("price"+price);
+        System.out.println("name"+name);
+        System.out.println("isStudent"+isStudent);
+
+    }
+}
