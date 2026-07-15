74b5a1862b48100455b2d7b00fdc633f6d983934
RITESH26-arch <rdongave2@gmail.comgit config --global --listgit config --global user.email rdongave2@gmail.com>
2026-07-16 01:52:13 +0530

    Today Learned a new topic Nested Loops

--- NestedLoops/ReverseStarPattern.java
+++ NestedLoops/ReverseStarPattern.java
@@ -3,7 +3,7 @@ public class ReverseStarPattern {
         for(int i=0;i<=5;i++){
             for(int j=5;j>=i;j--){
                 System.out.print("*");
-            }
+            } 
             System.out.println();
         }
     
