e2afc574f21861cdb34d19bfc6564437bdb8b5cd
RITESH26-arch <rdongave2@gmail.com>
2026-08-23 01:47:28 +0530

    Learned Abstraction and tried implementing it in code, recorganized what kind of errors can occur in abstraction

---
+++ OOPS/Abstraction/Car.java
@@ -0,0 +1,65 @@
+public class Car extends Vehical {
+    private String name;
+    private String type;
+    private String carNumber;
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    void setType(String type){
+        this.type = type;
+    }
+
+    void setCarNumber(String carNumber){
+        int i = 0;
+        boolean valid = true;
+        while(i<carNumber.length()){
+            if(Character.isWhitespace(carNumber.charAt(i) ) != true && Character.isLetterOrDigit(carNumber.charAt(i)) != true){
+                valid = false;
+                break;
+            }
+            else 
+                i++;
+        }
+
+        if(valid == true)
+            this.carNumber = carNumber;
+        else
+            System.out.println( "\n" +carNumber + " -- INVALID  CAR NUMBER ! \nPlease enter a valid car number");
+    }
+
+
+    Car(String brand,int noOfDoors,String name,String type,String carNumner){
+        super(brand, noOfDoors);
+        setName(name);
+        setType(type);
+        setCarNumber(carNumner);
+    }
+
+    Car(String brand,int noOfDoors,String name,String type){
+        super(brand, noOfDoors);
+        setName(name);
+        setType(type);
+    }
+
+    Car(String brand,int noOfDoors,String name){
+        super(brand, noOfDoors);
+        setName(name);
+    }
+
+    void Display(){
+        super.Display();
+        System.out.println("Name => " + name + "\nType => " + type + "\nCar number => " +carNumber);
+    }
+
+    @Override
+    void startEngine(){
+        System.out.println("Vrroom Vrrroooommmm !!!!");
+    }
+
+    
+    
+    
+}
+
---
+++ OOPS/Abstraction/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Vehical v1 = new Car("Honda",2,"Accord","Sedan","mh 12 evv 1251");
+        v1.startEngine();
+        Vehical v2 = new Vehical();
+    }
+}
