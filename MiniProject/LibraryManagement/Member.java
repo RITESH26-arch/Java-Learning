d0b83c0e6251579b48f1a01c955eabf01ec035ad
RITESH26-arch <rdongave2@gmail.com>
2026-09-10 13:12:58 +0530

    Working on Library management system

---
+++ MiniProject/LibraryManagement/Member.java
@@ -0,0 +1,45 @@
+abstract class Member {
+    private int memberId;
+    private String memberName;
+
+    abstract double calculateFine();
+
+    void displayiMemberInfo(){
+        System.out.println("\nMember Id => " + memberId + "\nMember Name => " + memberName);
+    }
+
+    void setMemberID(int memberId){
+        if(memberId <= 0){
+            System.out.println("Invalid Member Id !! \nPlease enter a valid Member Id");
+        }
+        else{
+            this.memberId = memberId;
+        }
+    }
+
+    void setMemberName(String memberName){
+        int i=0;
+        boolean flag = true;
+        while(i<memberName.length()){
+            if(Character.isDigit(memberName.charAt(i))){
+                flag = false;
+                break;
+            }
+            else{
+                i++;
+            }
+        }
+        if(flag==true){
+                this.memberName = memberName;
+            }
+            else{
+                System.out.println("Digits are not allowed in the name !");
+            }
+    }
+
+    Member(int memberId,String memberName){
+        setMemberID(memberId);
+        setMemberName(memberName);
+    }
+    
+}
